dbcfcd53948e3cd7aef3bdf49cec5417472e7232
Walter Kagwima <walterkagwima2019@gmail.com>
2023-11-19 14:09:58 +0300

    second review

--- server/db.js
+++ server/db.js
@@ -1,9 +1,16 @@
-//Connection to database
 import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
-  mongoose.set("strictQuery", false);
-  const connect = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`Mongo db connected: ${connect.connection.host}`);
+  try {
+    mongoose.set("strictQuery", false);
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log(`MongoDb Connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
 };
+
 export default connectToDatabase;
--- server/index.js
+++ server/index.js
@@ -5,16 +5,14 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 
-//Routes
+// Routes
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import stripeRoute from "./routes/stripeRoute.js";
 import orderRoutes from "./routes/orderRoutes.js";
 
 connectToDatabase();
-
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
@@ -27,17 +25,23 @@ app.get("/api/config/google", (req, res) =>
   res.send(process.env.GOOGLE_CLIENT_ID)
 );
 
+const port = 5000;
+
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
 if (process.env.NODE_ENV == "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
+
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
 
-const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Api is running...");
+});
 
 app.listen(port, () => {
-  console.log(`Server runs on port: ${port}.`);
+  console.log(`Server runs on port ${port}`);
 });
