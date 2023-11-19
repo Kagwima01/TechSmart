d77504f5195c30bbc90790072460b1e9ca280cbe
Walter Kagwima <walterkagwima2019@gmail.com>
2023-11-19 10:52:18 +0300

    fourth commit

--- server/db.js
+++ server/db.js
@@ -2,12 +2,8 @@
 import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
-  try {
-    mongoose.set("strictQuery", false);
-    const connect = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Mongo db connected: ${connect.connection.host}`);
-  } catch (error) {
-    console.log(`Error: ${error.message}`);
-  }
+  mongoose.set("strictQuery", false);
+  const connect = await mongoose.connect(process.env.MONGO_URI);
+  console.log(`Mongo db connected: ${connect.connection.host}`);
 };
 export default connectToDatabase;
